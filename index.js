1467ae40b6d0a5b03b07576b468f627ef24da861
Jie He <jihe@conviva.com>
2020-06-09 15:32:13 +0800

    Complete main function.

--- index.js
+++ index.js
@@ -1,48 +1,63 @@
-const fs = require('fs')
-const path = require('path')
-const Table = require('cli-table')
-const colors = require('colors')
-const brotli = require('brotli')
-const zlib = require('zlib')
-const { promisify } = require('util')
+const fs = require("fs")
+const path = require("path")
+const Table = require("cli-table")
+const colors = require("colors")
+const brotli = require("brotli")
+const zlib = require("zlib")
+const { promisify } = require("util")
 
-const brotliSettings = {
-  extension: 'br',
-  skipLarger: true,
-  mode: 1, // 0 = generic, 1 = text, 2 = font (WOFF2)
-  quality: 11, // 0 - 11,
-  lgwin: 12, // default
-}
-
-const gzipSettings = {
+const zlibSettings = {
   level: 9,
 }
 
-const extArr = ['.js', '.css', '.json', '.html']
-function compression(file) {
-  if (file.endsWith('.js') || file.endsWith('.css') || file.endsWith('.html')) {
-    // brotli
-    const result = brotli.compress(fs.readFileSync(file), brotliSettings)
-    fs.writeFileSync(file + '.br', result)
-    // gzip
-    const fileContents = fs.createReadStream(file)
-    const writeStream = fs.createWriteStream(file + '.gz')
-    const zip = zlib.createGzip(gzipSettings)
-    fileContents
-      .pipe(zip)
-      .on('error', (err) => console.error(err))
-      .pipe(writeStream)
-      .on('close', (err) => console.log('done'))
-      .on('error', (err) => console.error(err))
-  }
+const extArr = [".js", ".css", ".json", ".html", ".xml", ".woff2"]
+
+function compressToGzip(file) {
+  return new Promise((resolve, reject) => {
+    const ext = path.extname(file)
+    const res = file + ".gz"
+    if (extArr.indexOf(ext) !== -1) {
+      const fileContents = fs.createReadStream(file)
+      const writeStream = fs.createWriteStream(res)
+      const zip = zlib.createGzip(zlibSettings)
+      fileContents
+        .pipe(zip)
+        .on("error", err => reject(err))
+        .pipe(writeStream)
+        .on("finish", () => resolve(res))
+        .on("error", err => reject(err))
+    } else {
+      resolve()
+    }
+  })
+}
+
+function compressToBrotli(file) {
+  return new Promise((resolve, reject) => {
+    const ext = path.extname(file)
+    const res = file + ".br"
+    if (extArr.indexOf(ext) !== -1) {
+      const fileContents = fs.createReadStream(file)
+      const writeStream = fs.createWriteStream(res)
+      const br = zlib.createBrotliCompress(zlibSettings)
+      fileContents
+        .pipe(br)
+        .on("error", err => reject(err))
+        .pipe(writeStream)
+        .on("finish", () => resolve(res))
+        .on("error", err => reject(err))
+    } else {
+      resolve()
+    }
+  })
 }
 
 const table = new Table({
   head: [
-    colors.green('Text files'),
-    colors.green('Size(text files)'),
-    colors.green('Size(gzip files)'),
-    colors.green('Size(brotli files)'),
+    colors.green("Text files"),
+    colors.green("Size(text files)"),
+    colors.green("Size(gzip files)"),
+    colors.green("Size(brotli files)"),
   ],
   colWidths: [70, 30, 30, 30],
 })
@@ -71,17 +86,53 @@ const walk = function (dir, exts, done) {
   })
 }
 
-const p = path.join(__dirname, '.')
+const p = path.join(__dirname, ".")
 walk(p, extArr, cb)
 
 function cb(err, res) {
-  const size = res
-    .map((e) => getFilesizeInBytes(e))
-    .reduce((ac, e) => ac + +e, 0)
-  const arr = res.map((e) => e.slice(__dirname.length + 1))
-  table.push([arr.join('\n'), sizeFn(size)])
-  console.log(table.toString())
+  const size = res.map(e => getFilesizeInBytes(e)).reduce((ac, e) => ac + +e, 0)
+  const arr = res.map(e => e.slice(__dirname.length + 1))
+  let p = Promise.resolve()
+  const bSize = []
+  const gSize = []
+  for (let i = 0, len = res.length; i < len; i++) {
+    p = p.then(() => {
+      return compressToBrotli(res[i]).then(f => {
+        const s = getFilesizeInBytes(f)
+        bSize[i] = s
+        return s
+      })
+    })
+  }
+  for (let i = 0, len = res.length; i < len; i++) {
+    p = p.then(() => {
+      return compressToGzip(res[i]).then(f => {
+        const s = getFilesizeInBytes(f)
+        gSize[i] = s
+        return s
+      })
+    })
+  }
+  p.then(() => {
+    const gSizeSum = gSize.reduce((ac, e) => ac + +e, 0)
+    const bSizeSum = bSize.reduce((ac, e) => ac + +e, 0)
+    table.push([
+      arr.join("\n"),
+      sizeFn(size),
+      sizeFn(gSizeSum) + `(${delta(size, gSizeSum)})`,
+      sizeFn(bSizeSum) + `(${delta(gSizeSum, bSizeSum)})`,
+    ])
+    console.log(table.toString())
+  })
 }
+
+function delta(a, b) {
+  const sign = a > b ? "-" : a === b ? "" : "+"
+  const num = ((b - a) / a) * 100
+  const res = num.toFixed(2) + '%'
+  return a > b ? colors.green(res) : a === b ? res : colors.red(res)
+}
+
 function sizeFn(num) {
   if (num / 1024 >= 1) {
     return `${(num / 1024).toFixed(2)}MB`
@@ -92,8 +143,6 @@ function sizeFn(num) {
 
 function getFilesizeInBytes(filename) {
   const stats = fs.statSync(filename)
-  const fileSizeInKiloBytes = (stats['size'] / 1024).toFixed(2)
+  const fileSizeInKiloBytes = (stats["size"] / 1024).toFixed(2)
   return fileSizeInKiloBytes
 }
-
-compression(path.join(__dirname, 'index.js'))
